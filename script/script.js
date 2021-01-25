ec4d86f53609eaf8d599c908479ef8be0fb8bc30
deepinmyr <77839491+deepinmyr@users.noreply.github.com>
2021-01-25 10:32:29 +0800

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,16 @@
+function onClick(){
+    alert("message");
+}
+
+function onMouseover(){
+    alert("Over");
+}
+
+function omt(){
+    alert("out");
+}
+
+function onst(){
+    s = document.getElementById("text");
+    alert(s.value);
+}
